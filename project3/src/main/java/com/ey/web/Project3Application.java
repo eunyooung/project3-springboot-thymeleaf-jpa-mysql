e94f8cb1e1f0d9858a1115e42dda829b38206396
eunyooung <choi327391@gmail.com>
2022-05-05 09:14:43 +0900

    AWS 배포 설정

--- project3/src/main/java/com/ey/web/Project3Application.java
+++ project3/src/main/java/com/ey/web/Project3Application.java
@@ -2,13 +2,31 @@ package com.ey.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.ey.web.controller","com.ey.web.entity","com.ey.web.dao"})
-public class Project3Application {
+public class Project3Application extends SpringBootServletInitializer {
+    
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Project3Application.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(Project3Application.class, args);
 	}
 }
+/*
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.ey.web.controller","com.ey.web.entity","com.ey.web.dao"})
+public class Project3Application {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(Project3Application.class, args);
+    }
+}
+*/
